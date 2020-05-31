596347ea172caaad021d97365c1a37cc82a6f60e
sammyne <lixiangmin01@baidu.com>
2020-05-31 07:14:01 +0000

    test: make use of the cargo-teaclave

--- tests/sgx/README.md
+++
@@ -1,5 +0,0 @@
-# Hello World example without CMake
-
-## Progress
-- [x] Build the app with cargo only 
-- [ ] Build the enclave with cargo only 
--- tests/sgx/enclave/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![no_std]
-
-extern crate sgx_tstd as std;
-
-#[no_mangle]
-pub extern "C" fn ecall_do_rsgx_tests() -> u64 {
-    !wheel::tests::run() as u64
-}
